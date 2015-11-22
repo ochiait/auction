3a675a734d4a3d91f98944911cf540b0d4e3ee70
Tetsuya Ochiai <ochiai@ryorika-mm.jp>
2015-11-22 12:14:33 +0900

    Day2午前_2-5 ビュー開発(index)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,7 @@ class ItemsController < ApplicationController
     #Itemというモデルに対してfindメソッドでidを検索させている
     @item = Item.find(params[:id])
   end
+  def index
+    @items = Item.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 get '/items/:id' => 'items#show'
+get '/items/' => 'items#index'
 end
